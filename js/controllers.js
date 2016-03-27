67878c8a9ccde77667325088ab1ad1916d2ba2f8
Jesla <jesslar415@gmail.com>
2016-03-27 11:38:40 -0500

    6 of 6 Complete

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
-var controllers = angular.module('simpleTasksApp', []);
-controllers.controller('simpleTasksController', function ($scope) {
+var controllers = angular.module("simpleTasksApp", []);
+controllers.controller("simpleTasksController", function ($scope) {
     $scope.firstObjective = function () {
-        alert('One Down! Five to Go!')
+        alert("One Down! Five to Go!")
     };
 
-    $scope.secondObjective = function () { //COME BACK TO THIS AND ADD ERROR ALERT IF INPUT IS EMPTY
+    $scope.secondObjective = function () { //FIGURE OUT HOW TO ADD ERROR ALERT IF INPUT IS EMPTY
        alert($scope.secondObjInput)
     };
         
@@ -12,21 +12,20 @@ controllers.controller('simpleTasksController', function ($scope) {
         $scope.colorChange = {backgroundColor: "red"};
     };
     
-    $scope.thirdObjRev = function () {  // I FEEL LIKE THIS IS A CHEAP WAY TO DO THIS?
-        $scope.colorChange = {backgroundColor: ''};
+    $scope.thirdObjRev = function () {  // I FEEL LIKE THIS IS KIND OF A CHEAP WAY TO DO THIS?
+        $scope.colorChange = {backgroundColor: ""};
     };
         
     $scope.fourthObjective = function () {
         $scope.textColorChange = {color: "#" + (Math.random() * 0xFFFFFF << 0).toString(16)}
     };
         
+    $scope.fifthObjective = function () {
+        $scope.fifthObjName = "Jessica Larsen";
+    };
       
-      
-// 5. Add a button and an empty div. When the button is clicked, add a `span` that contains your 
-// name to the empty div.
-
-//6. Create a button and a `ul` in your HTML. In JavaScript, create an array containing the names 
-// of your friends (at least 10. If you don't have that many, include your imaginary ones). When
-// the button is clicked, add each friend's name as an `li` to the `ul` on the page.
+    $scope.sixthObjective = function() {
+        $scope.friends = ["Stacy", "JD", "Jared", "Brian", "Kinley", "Blair", "Stephanie", "Susan", "Jim", "Mark"];
+    };
         
 });
